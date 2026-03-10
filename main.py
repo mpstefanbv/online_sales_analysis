e4f9fa0f6272c7f0e02e9539979671f562ec555d
Mircea-Precup Stefan <mpstbv@gmail.com>
2026-03-10 21:21:55 +0200

    Adauga clasa Cart si functionalitatea de cos in main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
+import random
 
 def main():
     manager = ProductManager()
@@ -7,16 +9,27 @@ def main():
     p1 = Product("Laptop", 3500.0, 5)
     p2 = Product("Mouse", 80.0, 20)
     p3 = Product("Tastatura", 150.0, 10)
+    p4 = Product("Monitor", 900.0, 7)
 
     manager.add_product(p1)
     manager.add_product(p2)
     manager.add_product(p3)
+    manager.add_product(p4)
 
     print("Produse disponibile:")
     manager.display_products()
+    print(f"Valoarea totala a inventarului: {manager.total_inventory_value()}")
+    
+    cart = Cart()
+    
+    available_products = manager.products
+    selected_products = random.sample(available_products, 3)
 
-    total_value = manager.total_inventory_value()
-    print(f"Valoarea totala a inventarului: {total_value}")
+    for prod in selected_products:
+        cart.add_to_cart(prod)
+
+    cart.display_cart()
+    print(f"Valoarea totala de plata a cosului: {cart.total_cart_value()}")
 
 if __name__ == "__main__":
     main()
